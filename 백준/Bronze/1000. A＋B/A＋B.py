33b70de764d679d58d22c8ed8dfd064cc8693a30
jiixeon <jiyeon070522@icloud.com>
2026-02-18 00:04:42 +0900

    [Bronze V] Title: A+B, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.py
+++ 백준/Bronze/1000. A＋B/A＋B.py
@@ -1,5 +1,9 @@
 #백준 2557번: A+B 출력
 #두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
-A,B = map(int,input().split()) #두 값을 받으니 map으로, int-숫자형, split-()안에 있는거 기준으로 나누기
-print(A+B)
+a = list(map(int,input().split())) 
+print(a[0]+a[1])
+
+#리스트
+#list - 리스트란 문자나, 숫자를 일정한 형태를 가지고 그룹화 시키는 것
+#첫번째 값은 0번방임
